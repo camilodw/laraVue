207ac3b98fffbb4e45aa5d25aa95063d84c933f9
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-12-05 15:53:33 -0600

    se agregaron rutas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\GameController;
 
 
 /*
@@ -19,6 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('api')->group(function () {
-    Route::resource('games', GameController::class);
-});
+
+Route::Resource('games', GameController::class);

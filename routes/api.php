b7bf7a137c39c846cf3351f0aa0dde4aa2594111
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-12-09 10:31:34 -0600

    se edito ruta

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::Resource('games', GameController::class);
+Route::apiResource('games', GameController::class);
